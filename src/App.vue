a3e76dc92f1005c2334d9a16ab2a6b8344c13ed6
CHUNDOOIN <chun.doo.in@gmail.com>
2022-08-24 16:56:08 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
             <div class="btn-wrap">
                 <button @click="likeDataShow()" class="btn">찜 목록</button>
                 <button @click="sortDataShow()" class="btn">가격순</button>
-                <button @click="itemData = originalItemData" class="btn">
+                <button @click="originalItemDataShow()" class="btn">
                     전체보기
                 </button>
             </div>
@@ -15,45 +15,44 @@
         <!-- 아이템 리스트 부분 -->
         <div class="list-wrap">
             <div class="item-wrap" v-for="(item, idx) in itemData" :key="idx">
-                <!-- {{ item.title }} -->
+                <div class="item-box-wrap" @click="moveDetailPage(item)">
+                    <div class="item-wrap-left">
+                        <div
+                            class="item-image"
+                            :style="`background-image: url(${item.image})`"
+                        ></div>
+                    </div>
 
-                <div class="item-wrap-left">
+                    <div class="item-wrap-right">
+                        <div class="item-line">
+                            <div class="item-text">상품명</div>
+                            <span class="item-content-text">{{
+                                item.title
+                            }}</span>
+                        </div>
+
+                        <div class="item-line">
+                            <div class="item-text">가격</div>
+                            <span class="item-content-text"
+                                >{{ item.price.toLocaleString() }}원</span
+                            >
+                        </div>
+                    </div>
+                </div>
+                <div class="like-icon-wrap">
                     <div
-                        class="item-image"
-                        :style="`background-image: url(${item.image})`"
-                    ></div>
-                </div>
-
-                <div class="item-wrap-right">
-                    <div class="item-line">
-                        <div class="item-text">상품명</div>
-                        <span class="item-content-text">{{ item.title }}</span>
+                        @click="clickLike(idx, item)"
+                        v-if="item.like === false"
+                        class="item-basket"
+                    >
+                        🖤
                     </div>
-
-                    <div class="item-line">
-                        <div class="item-text">가격</div>
-                        <span class="item-content-text"
-                            >{{ item.price.toLocaleString() }}원</span
-                        >
-                    </div>
-                </div>
-
-                <div>
-                    <div class="item-line">
-                        <div
-                            @click="clickLike(idx, item)"
-                            v-if="item.like === false"
-                            class="item-basket"
-                        >
-                            🖤
-                        </div>
-                        <div
-                            v-if="item.like === true"
-                            @click="clickLike(idx, item)"
-                            class="item-basket"
-                        >
-                            🧡
-                        </div>
+                    <div
+                        v-if="item.like === true"
+                        @click="clickLike(idx, item)"
+                        class="item-basket"
+                    >
+                        🧡
                     </div>
                 </div>
             </div>
@@ -62,15 +61,17 @@
 </template>
 
 <script>
-import itemData from "./assets/data.js";
+import data from "./assets/data.js";
 
 export default {
     name: "App",
     data() {
         return {
-            itemData: itemData,
-            originalItemData: [...itemData],
+            itemData: data,
+            originalItemData: [...data],
             likeData: [],
+
+            nowTab: 0,
         };
     },
     methods: {
@@ -85,11 +86,17 @@ export default {
         },
         likeDataShow() {
             this.itemData = this.itemData.filter((el) => el.like === true);
+            this.nowTab = 2;
         },
         sortDataShow() {
             this.itemData = this.itemData.sort((a, b) =>
                 a.price > b.price ? 1 : -1
             );
+            this.nowTab = 1;
+        },
+        originalItemDataShow() {
+            this.itemData = this.originalItemData;
+            this.nowTab = 0;
         },
     },
 };
