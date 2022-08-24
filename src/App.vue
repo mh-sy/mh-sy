1826d2c5504cfc7d0a5164d7aac917c3212c6adc
CHUNDOOIN <chun.doo.in@gmail.com>
2022-08-24 14:06:14 +0900

    최초 업로드

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="page-wrap">
+        <!-- 헤더 부분 -->
+        <div class="header-wrap">
+            <div class="logo-text">2buy2go</div>
+            <div class="btn-wrap">
+                <button @click="likeDataShow()" class="btn">찜 목록</button>
+                <button @click="sortDataShow()" class="btn">가격순</button>
+                <button @click="itemData = originalItemData" class="btn">
+                    전체보기
+                </button>
+            </div>
+        </div>
+
+        <!-- 아이템 리스트 부분 -->
+        <div class="list-wrap">
+            <div class="item-wrap" v-for="(item, idx) in itemData" :key="idx">
+                <!-- {{ item.title }} -->
+
+                <div class="item-wrap-left">
+                    <div
+                        class="item-image"
+                        :style="`background-image: url(${item.image})`"
+                    ></div>
+                </div>
+
+                <div class="item-wrap-right">
+                    <div class="item-line">
+                        <div class="item-text">상품명</div>
+                        <span class="item-content-text">{{ item.title }}</span>
+                    </div>
+
+                    <div class="item-line">
+                        <div class="item-text">가격</div>
+                        <span class="item-content-text"
+                            >{{ item.price.toLocaleString() }}원</span
+                        >
+                    </div>
+                </div>
+
+                <div>
+                    <div class="item-line">
+                        <div
+                            @click="clickLike(idx, item)"
+                            v-if="item.like === false"
+                            class="item-basket"
+                        >
+                            🖤
+                        </div>
+                        <div
+                            v-if="item.like === true"
+                            @click="clickLike(idx, item)"
+                            class="item-basket"
+                        >
+                            🧡
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import itemData from "./assets/data.js";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            itemData: itemData,
+            originalItemData: [...itemData],
+            likeData: [],
+        };
+    },
+    methods: {
+        moveDetailPage(item) {
+            location.href = item.url;
+            // console.log(item);
+        },
+
+        clickLike(idx, item) {
+            // console.log("내 인덱스는?", idx, item);
+            item.like = !item.like;
+        },
+        likeDataShow() {
+            this.itemData = this.itemData.filter((el) => el.like === true);
+        },
+        sortDataShow() {
+            this.itemData = this.itemData.sort((a, b) =>
+                a.price > b.price ? 1 : -1
+            );
+        },
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/assets/data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+        number: 0,
+        title: "KBDiy XDA 프로필 PBT Keycaps 137 키/Apple MAC 용 ISO Cherry MX DIY 맞춤형 기계식 키보드 용 일본어 흰색 Keycap xda 키캡 애플 키캡 레트로 키캡 포인트 키캡 맥 키캡 애플키캡 맥용 키캡",
+        price: 18482,
+        image: "https://ae01.alicdn.com/kf/A7be17a8f65124ecc99d0657e0065d08ba/KBDiy-XDA-Profile-PBT-Keycaps-137-Keys-Set-For-Apple-MAC-ISO-Cherry-MX-Japanese-White.jpg_220x220xz.jpg_.webp",
+        url: "https://ko.aliexpress.com/item/1005004065661267.html?spm=a2g0o.productlist.0.0.1e3e15b9WEuogQ&algo_pvid=11c7d7bd-c2a0-439c-8271-c2ac3ae04ac9&aem_p4p_detail=2022082317091817897242551800023591898&algo_exp_id=11c7d7bd-c2a0-439c-8271-c2ac3ae04ac9-0&pdp_ext_f=%7B%22sku_id%22%3A%2212000027923659515%22%7D&pdp_npi=2%40dis%21KRW%2137502.0%2118482.0%21%21%21%21%21%4021031a5516612997579137309e93ad%2112000027923659515%21sea&curPageLogUid=am1t7Ojh1kDC&ad_pvid=2022082317091817897242551800023591898_1",
+        like: false,
+    },
+
+    {
+        number: 1,
+        title: "KBDiy GMK Keycap Clone Olivia Arctic Red Blue Samurai Darling Rudy OEM Profile Double Shot PBT Keycaps for Mechanicla Keyboard",
+        price: 30043,
+        image: "https://ae01.alicdn.com/kf/S0c97da1175784ef79ab7dbfb12f38347n/KBDiy-GMK-Keycap-Clone-Olivia-Arctic-Red-Blue-Samurai-Darling-Rudy-OEM-Profile-Double-Shot-PBT.jpg_220x220xz.jpg_.webp",
+        url: "https://ko.aliexpress.com/item/1005004561250282.html?spm=a2g0o.productlist.0.0.1e3e15b9WEuogQ&algo_pvid=11c7d7bd-c2a0-439c-8271-c2ac3ae04ac9&aem_p4p_detail=2022082317091817897242551800023591898&algo_exp_id=11c7d7bd-c2a0-439c-8271-c2ac3ae04ac9-1&pdp_ext_f=%7B%22sku_id%22%3A%2212000029623156969%22%7D&pdp_npi=2%40dis%21KRW%2140057.0%2130043.0%21%21%21%21%21%4021031a5516612997579137309e93ad%2112000029623156969%21sea&curPageLogUid=myHUmBYgy6Zb&ad_pvid=2022082317091817897242551800023591898_2",
+        like: false,
+    },
+
+    {
+        number: 2,
+        title: "러시아어 스페인 미국 프랑스어 Ltalian 독일어 영국 레이아웃 무선 키보드 및 마우스 콤보 침묵 마우스 PC 노트북, 컴퓨터 Windows.",
+        price: 30300,
+        image: "https://ae01.alicdn.com/kf/Hc9b1d08babc74af480ed04ee8999dc23p/Russian-Spain-USA-French-Ltalian-German-UK-layout-Wireless-Keyboard-and-Mouse-Combo-Silent-Mice-for.jpg_220x220xz.jpg_.webp",
+        url: "https://ko.aliexpress.com/item/4000402696745.html?spm=a2g0o.productlist.0.0.1e3e15b9WEuogQ&algo_pvid=11c7d7bd-c2a0-439c-8271-c2ac3ae04ac9&aem_p4p_detail=2022082317091817897242551800023591898&algo_exp_id=11c7d7bd-c2a0-439c-8271-c2ac3ae04ac9-4&pdp_ext_f=%7B%22sku_id%22%3A%2210000002921734877%22%7D&pdp_npi=2%40dis%21KRW%2161771.0%2137060.0%21%21%21%21%21%4021031a5516612997579137309e93ad%2110000002921734877%21sea&curPageLogUid=J77GqP5jp9ip&ad_pvid=2022082317091817897242551800023591898_5",
+        like: false,
+    },
+
+    {
+        number: 3,
+        title: "무선 기계식 키보드 초박형 104 키 단일 백라이트 지원 블루투스/타입-c, 안드로이드 윈도우 데스크탑 노트북 PC 용",
+        price: 76027,
+        image: "https://ae01.alicdn.com/kf/Hecf38f8feebd4a2ebe55665704e8ea29o/Wireless-Mechanical-Keyboard-Ultra-Thin-104-Keys-Single-Backlight-Support-Bluetooth-Type-c-for-Android-Windows.jpg_220x220xz.jpg_.webp",
+        url: "https://ko.aliexpress.com/item/1005003235348690.html?spm=a2g0o.productlist.0.0.7cc415b9UzuqvT&algo_pvid=c59f30cb-ed5b-4050-91fe-3356eda0d128&aem_p4p_detail=202208232128512326309597647000000554037&algo_exp_id=c59f30cb-ed5b-4050-91fe-3356eda0d128-0&pdp_ext_f=%7B%22sku_id%22%3A%2212000028500583084%22%7D&pdp_npi=2%40dis%21KRW%21138231.0%2176027.0%21%21%21%21%21%402101d91e16613153311471858e9f9a%2112000028500583084%21sea&curPageLogUid=fAqjnYUjq5CK&ad_pvid=202208232128512326309597647000000554037_1",
+        like: false,
+    },
+
+    {
+        number: 4,
+        title: "RK ROYAL KLUDGE RK100 2.4G 무선/블루투스/유선 RGB 기계식 키보드, 100 키 3 가지 모드 연결 가능 핫 스왑 가능 화이트 Sw",
+        price: 72557,
+        image: "https://ae01.alicdn.com/kf/Sbfd376870cb94dd89a95c69542405eebW/RK-ROYAL-KLUDGE-RK100-2-4G-Wireless-Bluetooth-Wired-RGB-Mechanical-Keyboard-100-Keys-3-Modes.jpg_220x220xz.jpg_.webp",
+        url: "https://ko.aliexpress.com/item/1005003951327993.html?spm=a2g0o.productlist.0.0.7cc415b9UzuqvT&algo_pvid=c59f30cb-ed5b-4050-91fe-3356eda0d128&aem_p4p_detail=202208232128512326309597647000000554037&algo_exp_id=c59f30cb-ed5b-4050-91fe-3356eda0d128-5&pdp_ext_f=%7B%22sku_id%22%3A%2212000027546015240%22%7D&pdp_npi=2%40dis%21KRW%21108294.0%2172557.0%21%21%21%21%21%402101d91e16613153311471858e9f9a%2112000027546015240%21sea&curPageLogUid=0fuWpNuAfNfg&ad_pvid=202208232128512326309597647000000554037_6",
+        like: false,
+    },
+
+    {
+        number: 5,
+        title: "SeenDa-미니 블루투스 키보드, 안드로이드 iOS 윈도우즈 호환 스마트 TV 프로젝터용 터치 패드",
+        price: 15715,
+        image: "https://ae01.alicdn.com/kf/S2418f37ca3cc41dc9191c7a6bb54a72eT/SeenDa-Mini-Bluetooth-Keyboard-with-Touchpad-for-Smart-TV-Projector-Compatible-with-Android-iOS-Windows.jpg_220x220xz.jpg_.webp",
+        url: "https://ko.aliexpress.com/item/33021242524.html?spm=a2g0o.productlist.0.0.7cc415b9UzuqvT&algo_pvid=c59f30cb-ed5b-4050-91fe-3356eda0d128&algo_exp_id=c59f30cb-ed5b-4050-91fe-3356eda0d128-18&pdp_ext_f=%7B%22sku_id%22%3A%2210000000410911279%22%7D&pdp_npi=2%40dis%21KRW%2144343.0%2115715.0%21%21%21%21%21%402101d91e16613153311471858e9f9a%2110000000410911279%21sea&curPageLogUid=xRBY1QDmgEfL",
+        like: false,
+    },
+];
+
+export default data;
